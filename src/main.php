f7cfe8712273f25d3f7959b91a9185e6fd103e9c
d.berestean@unifun.com <d.berestean@unifun.com>
2021-04-09 15:55:50 +0300

    deleted wrong project

--- src/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include_once(BASE_PATH.'/includes/database.php');
-    include_once(BASE_PATH.'/includes/functions.php');
-
-    print_r(json_encode(check_request()));
-    //print_r(check_request());
